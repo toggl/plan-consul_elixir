ede3294dd4d65aeae1805d3ec89c66a7d0cc41ee
Artur Cygan <arczicygan@gmail.com>
2017-09-12 18:29:27 -0700

    Move registering to service module

--- lib/consul.ex
+++ lib/consul.ex
@@ -5,15 +5,6 @@ defmodule Consul do
 
   require Logger
 
-  def register_service(name, port, check) do
-    check = Map.merge(%{Interval: "10s"}, Enum.into(check, %{}))
-    payload = %{Name: name, Port: port, Check: check}
-    json_payload = Poison.encode!(payload)
-    Logger.info("Registering consul service with parameters: #{json_payload}")
-    HTTPoison.get(base_url("/agent/service/deregister/#{name}"))
-    HTTPoison.post(base_url("/agent/service/register"), json_payload)
-  end
-
   def connect_nodes(service_name) do
     for host <- Consul.Service.addresses(service_name),
       do: :"#{node_name()}@#{host}" |> Node.connect()
--- lib/consul/service.ex
+++ lib/consul/service.ex
@@ -1,4 +1,15 @@
 defmodule Consul.Service do
+  require Logger
+
+  def register(name, port, check) do
+    check = Map.merge(%{Interval: "10s"}, Enum.into(check, %{}))
+    payload = %{Name: name, Port: port, Check: check}
+    json_payload = Poison.encode!(payload)
+    Logger.info("Registering consul service with parameters: #{json_payload}")
+    HTTPoison.get(Consul.base_url("/agent/service/deregister/#{name}"))
+    HTTPoison.post(Consul.base_url("/agent/service/register"), json_payload)
+  end
+
   def addresses(name) do
     with {:ok, %{status_code: 200} = response} <- HTTPoison.get(Consul.base_url("/catalog/service/#{name}")),
          {:ok, parsed} <- Poison.decode(response.body) do
