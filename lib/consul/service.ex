735817ebafe07f83075f29a239b77ec64a9e2e16
Yuri Lukyanov <y.snaky@gmail.com>
2024-11-07 19:13:47 +0400

    Read ServiceAddress first

--- lib/consul/service.ex
+++ lib/consul/service.ex
@@ -13,7 +13,13 @@ defmodule Consul.Service do
   def addresses(name) do
     with {:ok, %{status_code: 200} = response} <- HTTPoison.get(Consul.base_url("/catalog/service/#{name}")),
          {:ok, parsed} <- Poison.decode(response.body) do
-      addresses = for node_info <- parsed, do: node_info["Address"]
+      addresses = for node_info <- parsed do
+        if node_info["ServiceAddress"] != "" do
+          node_info["ServiceAddress"]
+        else
+          node_info["Address"]
+        end
+      end
       {:ok, addresses}
     end
   end
